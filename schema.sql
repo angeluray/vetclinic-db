eb48cde5a6bac2c0c5404960d7f5bc1ed7e4f785
angeluray <aeucampos@gmail.com>
2022-10-21 00:39:20 -0400

    Update schema with latest tables

--- schema.sql
+++ schema.sql
@@ -32,3 +32,32 @@ ALTER TABLE animals ADD CONSTRAINT fk_speciesid_owners FOREIGN KEY (species_id) 
 ALTER TABLE animals ADD COLUMN owner_id INT;
 SELECT * FROM animals;
 ALTER TABLE animals ADD CONSTRAINT fk_ownerid_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation date NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+  vet_id INT NOT NULL,
+  specie_id INT NOT NULL,
+  PRIMARY KEY(species_id, vets_id),
+  CONSTRAINT fk_specializations_species FOREIGN KEY (species_id) REFERENCES  species(id),
+  CONSTRAINT fk_specializations_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  animal_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  date_of_visit date NOT NULL,
+  PRIMARY KEY (animal_id, vet_id),
+  CONSTRAINT fk_visits_animals FOREIGN KEY (animal_id) REFERENCES animals(id),
+  CONSTRAINT fk_vet_visits FOREIGN KEY (vet_id) REFERENCES vets(id) 
+);
+
+ALTER TABLE visits DROP CONSTRAINT visits_pkey;
+LTER TABLE visits ADD COLUMN id SERIAL PRIMARY KEY;
+SELECT * FROM visits;
